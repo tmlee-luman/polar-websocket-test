dc0938efd17da476693e0baa90ec716d9d183835
tmlee <tmlee@lumanlab.com>
2022-06-13 17:31:12 +0900

    변경

--- src/main/java/com/lumanlab/stompapptestserver/controller/SimpleWebSocketPubController.java
+++ src/main/java/com/lumanlab/stompapptestserver/controller/SimpleWebSocketPubController.java
@@ -9,6 +9,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -19,14 +20,14 @@ public class SimpleWebSocketPubController {
 
     private final SimpMessagingTemplate template;
 
-    @MessageMapping(value = "/test_channel/message/{id}")
-    public void message(@PathVariable Long id, MessageRequest messageRequest) {
+    @MessageMapping(value = "/test_channel/message/1")
+    public void message(MessageRequest messageRequest) {
         log.info("요청받은 메시지 -----> {}", messageRequest.getMessage());
-        template.convertAndSend("/sub/test_channel/message/" + id, messageRequest.getMessage() + "를 보냈습니다.");
+        template.convertAndSend("/sub/test_channel/message/" + 1, messageRequest.getMessage() + "를 보냈습니다.");
     }
 
     @PostMapping("/test_channel/message/send/{id}")
-    public ResponseEntity<Void> sendMessage(@PathVariable Long id, MessageRequest messageRequest) {
+    public ResponseEntity<Void> sendMessage(@PathVariable Long id, @RequestBody MessageRequest messageRequest) {
         log.info("REST API 요청 메시지 -----> {}", messageRequest.getMessage());
         template.convertAndSend("/sub/test_channel/message/" + id, messageRequest.getMessage() + "를 보냈습니다.");
         return ResponseEntity.ok()
